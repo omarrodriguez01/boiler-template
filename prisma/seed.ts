fa0b8edbb767cea51ef47db09bc89d9be1db423e
Omar <omardanniel@outlook.com>
2023-05-04 22:02:53 -0600

    initial commit

---
+++ prisma/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client'
+import { studentSeeder } from './seeders/studentsSeeder'
+
+
+const prisma = new PrismaClient()
+
+
+async function main() {
+    await studentSeeder();
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
