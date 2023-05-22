8a400aa979b35a734e4d97253fdd6294c291a678
Gerardo Mora <A00827128@tec.mx>
2023-05-21 23:35:37 -0600

    added seeders

--- prisma/seeders/studentsSeeder.ts
+++ prisma/seeders/studentsSeeder.ts
@@ -5,19 +5,76 @@ const prisma = new PrismaClient()
 
 
 export const studentSeeder= async () => {
-    const student = await prisma.student.create({
-        data: {
-          id: 3,
-          firstName: 'Omar',
-          lastName: 'Rodriguez',
-          email: 'omar@omar.com',
-          phone: '661-890-1820',
-          address:  'Address',
-          gender : 'N/A',
-          dateOfBirth: new Date('1999-01-08 04:05:06'),
-          enrollmentDate: new Date('1999-01-08 04:05:06'),
-          graduationDate: new Date('1999-01-08 04:05:06'),
-        },
-      })
-  console.log({ student })
+  const country1 = await prisma.countries.create({
+    data: {
+      name: "Land of Fire",
+      citizen_amount: 10000,
+    },
+  })
+  console.log({ country1 })
+
+  const country2 = await prisma.countries.create({
+    data: {
+      name: "Land of Earth",
+      citizen_amount: 5000,
+    },
+  })
+  console.log({ country2 })
+
+  const country3 = await prisma.countries.create({
+    data: {
+      name: "Land of Iron",
+      citizen_amount: 10000,
+    },
+  })
+  console.log({ country3 })
+  
+  const village1 = await prisma.villages.create({
+    data: {
+      name: "Leaf Village",
+      country_id: 1,
+    },
+  })
+  console.log({ village1 })
+
+  const village2 = await prisma.villages.create({
+    data: {
+      name: "Rock Village",
+      country_id: 2,
+    },
+  })
+  console.log({ village2 })
+
+  const village3 = await prisma.villages.create({
+    data: {
+      name: "Hacho Village",
+      country_id: 1,
+    },
+  })
+  console.log({ village3 })
+
+
+  const ninja1 = await prisma.characters.create({
+      data: {
+        name: "Sasuke Uchiha",
+        village_id: 1,
+      },
+    })
+    console.log({ ninja1 })
+    
+    const ninja2 = await prisma.characters.create({
+      data: {
+        name: "Naruto Uzumaki",
+        village_id: 1,
+      },
+    })
+    console.log({ ninja2 })
+
+    const ninja3 = await prisma.characters.create({
+      data: {
+        name: "Kenji",
+        village_id: 2,
+      },
+    })
+    console.log({ ninja3 })
 }
